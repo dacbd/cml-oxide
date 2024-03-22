ce012978ce5a5e3e308022a53b594b8eeebab16b
Daniel Barnes <dabarnes2b@gmail.com>
2024-03-22 08:31:04 -0700

    most basic templating of subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands
+}
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command()]
+    Runner {}
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Not implemented yet!");
+    let args = Cli::parse();
+    match args.command {
+        Commands::Runner {} => {
+            println!("runner not implemented yet!")
+        }
+    }
     Ok(())
 }
